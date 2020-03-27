a1ce44bf0f1cd9714b58015122cc13a48093155e
Jaakko <j.i.mantyla@gmail.com>
2020-03-27 13:33:41 +0200

    adds mapping for posting svg file

--- src/main/java/com/jaakkomantyla/gcc/gcodecreator/GCCController.java
+++ src/main/java/com/jaakkomantyla/gcc/gcodecreator/GCCController.java
@@ -1,7 +1,58 @@
 package com.jaakkomantyla.gcc.gcodecreator;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.util.Optional;
+
+@CrossOrigin("*")
 @RestController
 public class GCCController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(GCCController.class);
+
+    @PostMapping(value = "/uploadSVG", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity uploadFile(@RequestParam MultipartFile file) {
+        logger.info(String.format("File name '%s' uploaded successfully.", file.getOriginalFilename()));
+        return ResponseEntity.ok().build();
+    }
+
+    /*
+    @RequestMapping(value = "/svgPrettyPrint", method = RequestMethod.POST)
+    public File svgPrettyPrint(@RequestBody PlayerInfo playerInfo){
+        String id = playerInfo.getId();
+        Optional<Score> score = highScoreRepository.findById(playerInfo.getId());
+
+        Integer points = score.get().getPoints();
+        String name =  score.get().getName();
+        if(points == null){
+            points = 0;
+        }
+        Optional<PushCounter> pushCounter= pushCountRepository.findById(1);
+        if(pushCounter.isPresent() && points>0){
+            points --;
+            int newCount= ((pushCounter.get().getCounter()+1)%500);
+            int pointsWon = ClickCountUtils.checkWin(newCount);
+            boolean win = !(pointsWon==0);
+            int distanceToNextPrice = ClickCountUtils.distanceToNextPrice(newCount);
+            pushCountRepository.setCountForPushCounter(newCount,1);
+            points +=pointsWon;
+            score.get().setPoints(points);
+            score.get().setName(name);
+            highScoreRepository.save(score.get());
+
+            return new PlayerInfo(points, distanceToNextPrice, win, name, id);
+        }else {
+            return new PlayerInfo(points, -1, false, name, id);
+        }
+
+    }
+
+     */
 }
